3da0f4cf646ef94fecacce3310dada64f6f1c762
liao <50760955+liaojunqi1816391@users.noreply.github.com>
2021-12-05 16:56:09 +0800

    Delete idea-git-test directory

--- idea-git-test/src/main/java/TestMain.java
+++
@@ -1,8 +0,0 @@
-public class TestMain {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("clone");
-        System.out.println("origin");
-    }
-}
